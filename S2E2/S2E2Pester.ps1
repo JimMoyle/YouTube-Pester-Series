a00a2ffb7c6e1978c70e996d7d6d0618e7059479
JimMoyle <git@jimmoyle.com>
2018-08-02 17:02:05 +0100

    Added demo lines

--- S2E2/S2E2Pester.ps1
+++ S2E2/S2E2Pester.ps1
@@ -7,25 +7,47 @@
 	===========================================================================
 #>
 
+#Requires -RunAsAdministrator
+
 # Current tested version and pre-reqs
 $psversiontable.PSVersion
-
-#See if Pester is there by default
-Get-Module -ListAvailable -Name Pester
-
-#Look for Pester in PSGallery
-Find-Module -Name Pester
-
-#Cert is different from PS Gallery so we need to do some additional parameters
-Install-Module -Name Pester -Force -SkipPublisherCheck
-
-#Now what do we see?
-Get-Module -ListAvailable -Name Pester
 
 #Just in case it's not already done.
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
 
-#Does Powershell import the righhtr version?
+#See if Pester is there by default
+Get-Module -ListAvailable -Name Pester
+
+#Pester on the web
+
+#Look for Pester in PSGallery
+$moduleInfo = Find-Module -Name Pester
+
+#what does it tell us?
+$moduleinfo | Format-List *
+
+#supported powershell version
+$moduleInfo.AdditionalMetadata.PowerShellVersion
+
+#look at documention
+Start-Process $moduleinfo.ProjectUri
+
+#look at PS Gallery
+Start-Process 'https://www.powershellgallery.com/packages/Pester/'
+
+#Cert is different from PS Gallery so we need to do some additional parameters
+Install-Module -Name Pester -Force -SkipPublisherCheck
+
+#Now you can update the module
+Update-Module -Name Pester
+
+#Now what do we see?
+Get-Module -ListAvailable -Name Pester
+
+#What does it look like on the filesystem?
+Invoke-Item -Path C:\Program Files\WindowsPowerShell\Modules\Pester
+
+#Does Powershell import the righht version?
 Import-Module -Name Pester
 
 #Quick look at the commands
