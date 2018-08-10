6739bd38ab206b2accc3dbe0d04ee578fb3800fb
JimMoyle <github@jimmoyle.com>
2018-08-10 13:45:55 +0100

    More Format changes

--- S2E3/Assertions/Compare-Me.Tests.ps1
+++ S2E3/Assertions/Compare-Me.Tests.ps1
@@ -7,7 +7,6 @@
 	===========================================================================
 #>
 
-
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . $here\$sut
